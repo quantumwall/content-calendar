0daba7dcd4bd0a46715406e32a0a28743f0a3562
quantumwall <liberiamor@gmail.com>
2023-09-18 16:46:56 +0300

    add spring empty project

---
+++ src/test/java/org/quantum/contentcalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.quantum.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
